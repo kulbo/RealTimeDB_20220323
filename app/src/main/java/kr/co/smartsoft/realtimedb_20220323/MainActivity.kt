fbb4259b7e59370d7a7413dac6df7f87e4ac6f95
kulbo <kulbo@hanmail.net>
2022-03-23 23:28:05 +0900

    DB 변경시 => 마지막으로 추가된 사항을 ChattingData로 변환/ 목록에 추가  - 변경 항목 저체 중 원하는 항목을 찾아가서 활용

--- app/src/main/java/kr/co/smartsoft/realtimedb_20220323/MainActivity.kt
+++ app/src/main/java/kr/co/smartsoft/realtimedb_20220323/MainActivity.kt
@@ -7,11 +7,15 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.smartsoft.realtimedb_20220323.datas.ChattingData
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : BaseActivity() {
     var messageCount = 0L // db에 저장된 채팅 갯수 Long 타입으로 저장
+
+    val mChattingList = ArrayList<ChattingData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +30,14 @@ class MainActivity : BaseActivity() {
             override fun onDataChange(snapshot: DataSnapshot) {
 //                snapshot 변수 : 현재 변경된 상태
                 messageCount = snapshot.childrenCount
+
+//                snapShot => 마지막 자여(최신 채팅 메세지) 추출 => ChattingData로 변환 + 목록 추가
+
+                mChattingList.add(
+                    ChattingData(
+                    snapshot.children.last().child("content").value.toString(),
+                    snapshot.children.last().child("createAt").value.toString()
+                ))
             }
 
             override fun onCancelled(error: DatabaseError) {
