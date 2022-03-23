ab52577ed28841447635d61cc95529ee367b2b32
kulbo <kulbo@hanmail.net>
2022-03-23 22:15:38 +0900

    채팅 UI 설정  - BaseActivity 에서 상속

--- app/src/main/java/kr/co/smartsoft/realtimedb_20220323/BaseActivity.kt
+++ app/src/main/java/kr/co/smartsoft/realtimedb_20220323/BaseActivity.kt
@@ -3,10 +3,12 @@ package kr.co.smartsoft.realtimedb_20220323
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
 abstract class BaseActivity : AppCompatActivity() {
     lateinit var mContext : Context
 
+    val realtimeDB = FirebaseDatabase.getInstance("https://realtimedb-20220323-default-rtdb.asia-southeast1.firebasedatabase.app/")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mContext = this
--- app/src/main/java/kr/co/smartsoft/realtimedb_20220323/MainActivity.kt
+++ app/src/main/java/kr/co/smartsoft/realtimedb_20220323/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.smartsoft.realtimedb_20220323
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +17,12 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+//        DB 연결
+        val db = FirebaseDatabase.getInstance("https://realtimedb-20220323-default-rtdb.asia-southeast1.firebasedatabase.app/")     // 싱가폴 db 주송
+//        DB 의 하위정보 설정
+        val testRef = db.getReference("test")
+
+        testRef.setValue("Hello World")
 
     }
 }
